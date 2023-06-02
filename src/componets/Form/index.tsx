be5d3255dd15dc39fa54557addc7c67ac1487c54
Lucas Xavier <lucassxxavier@gmail.com>
2023-06-01 22:20:15 -0300

    feat: form state

--- src/componets/Form/index.tsx
+++ src/componets/Form/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './form.css'
-import { FormControl, Grid, FormLabel, Input, FormHelperText, Switch,Heading, Button } from '@chakra-ui/react'
+import { FormControl, Grid, FormLabel, Input, FormHelperText, Switch, Heading, Button } from '@chakra-ui/react'
 import { User } from '../../App';
 
 interface IFormProps {
@@ -10,6 +10,10 @@ interface IFormProps {
 
 export default function Form({ form, setForm }: IFormProps) {
 
+    const onChangeInput = (e: any, key: keyof User) => {
+        setForm({ ...form, [key]: e.target.value })
+    }
+
     return (
         <div className='form-container'>
             <Heading as='h3' size='lg' marginBottom={10}>
@@ -18,35 +22,34 @@ export default function Form({ form, setForm }: IFormProps) {
             <Grid templateColumns='repeat(2, 1fr)' gap={6}>
                 <FormControl>
                     <FormLabel>Name</FormLabel>
-                    <Input type='text' />
+                    <Input onChange={e => { onChangeInput(e, "name") }} type='text' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
                     <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <Input onChange={e => { onChangeInput(e, "email") }} type='email' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
                     <FormLabel>Phone</FormLabel>
-                    <Input type='tel' />
+                    <Input onChange={e => { onChangeInput(e, "phone") }} type='tel' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
                     <FormLabel>Document</FormLabel>
-                    <Input type='text' />
+                    <Input onChange={e => { onChangeInput(e, "document") }} type='text' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
                     <FormLabel>Birthdate</FormLabel>
-                    <Input type='date' />
+                    <Input onChange={e => { onChangeInput(e, "birthdate") }} type='date' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
-                    <FormLabel>Birthdate</FormLabel>
-                    <Switch size='lg' h="40px"/>
+                    <FormLabel>You have cars ?</FormLabel>
+                    <Switch size='lg' h="40px" onChange={e => { setForm({ ...form, haveCar: e.target.checked }) }} />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
-                
             </Grid>
             <Button background="purple.700" color="white" size='lg' w="full" marginTop={10}>
                 Submit form

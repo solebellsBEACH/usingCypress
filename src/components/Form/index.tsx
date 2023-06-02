84bcefc123767e67e4a3cc694a71a34e8f5dd833
Lucas Xavier <lucassxxavier@gmail.com>
2023-06-02 10:40:54 -0300

    feat: finishing form and modal

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './form.css'
-import { FormControl, Grid, FormLabel, Input, FormHelperText, Switch, Heading, Button, Box, Text, } from '@chakra-ui/react'
+import { FormControl, SimpleGrid, FormLabel, Input, FormHelperText, Switch, Heading, Button, Box, Text, Modal, useDisclosure, ModalOverlay, ModalContent } from '@chakra-ui/react'
 import { User } from '../../App';
+import UserCard from '../UserCard';
 
 interface IFormProps {
     form: User;
@@ -9,59 +10,68 @@ interface IFormProps {
 }
 
 export default function Form({ form, setForm }: IFormProps) {
-
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const onChangeInput = (e: any, key: keyof User) => {
         setForm({ ...form, [key]: e.target.value })
     }
     const handleSubmit = () => {
-        console.log(form)
+        onOpen()
     }
     return (
-        <div className='form-container'>
-            <Heading as='h3' size='lg' marginBottom={10}>
-                Using Cypress
-            </Heading>
-            <Grid templateColumns='repeat(2, 1fr)' gap={6}>
-                <FormControl>
-                    <FormLabel>Name</FormLabel>
-                    <Input onChange={e => { onChangeInput(e, "name") }} type='text' />
-                    <FormHelperText>Type your name.</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input onChange={e => { onChangeInput(e, "email") }} type='email' />
-                    <FormHelperText>Type your email.</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Phone</FormLabel>
-                    <Input onChange={e => { onChangeInput(e, "phone") }} type='tel' />
-                    <FormHelperText>Type your phone.</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Document</FormLabel>
-                    <Input onChange={e => { onChangeInput(e, "document") }} type='text' />
-                    <FormHelperText>Type your document.</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Birthdate</FormLabel>
-                    <Input onChange={e => { onChangeInput(e, "birthdate") }} type='date' />
-                    <FormHelperText>Type your birthdate.</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>You have cars ?</FormLabel>
-                    <Box h="40px" alignItems='center' display='flex' flexDirection='row'>
-                    <Text>No</Text>
-                        <Switch marginLeft={5} marginRight={5} size='lg' onChange={e => { setForm({ ...form, haveCar: e.target.checked }) }} />
-                        <Text>Yes</Text>
-                    </Box>
-                    <FormHelperText>Select if you have a car.</FormHelperText>
-                </FormControl>
-            </Grid>
-            <Button
-                onClick={handleSubmit}
-                background="purple.700" color="white" size='lg' w="full" marginTop={10}>
-                Submit form
-            </Button>
-        </div>
+        <>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent alignItems='center'>
+                    <UserCard user={form} />
+                </ModalContent>
+            </Modal>
+            <div className='form-container'>
+                <Heading as='h3' size='lg' marginBottom={10}>
+                    Using Cypress
+                </Heading>
+                <SimpleGrid columns={[1, 2]} spacing='40px'>
+                    <FormControl>
+                        <FormLabel>Name</FormLabel>
+                        <Input onChange={e => { onChangeInput(e, "name") }} type='text' />
+                        <FormHelperText>Type your name.</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Email address</FormLabel>
+                        <Input onChange={e => { onChangeInput(e, "email") }} type='email' />
+                        <FormHelperText>Type your email.</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Phone</FormLabel>
+                        <Input onChange={e => { onChangeInput(e, "phone") }} type='tel' />
+                        <FormHelperText>Type your phone.</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Document</FormLabel>
+                        <Input onChange={e => { onChangeInput(e, "document") }} type='text' />
+                        <FormHelperText>Type your document.</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Birthdate</FormLabel>
+                        <Input onChange={e => { onChangeInput(e, "birthdate") }} type='date' />
+                        <FormHelperText>Type your birthdate.</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>You have cars ?</FormLabel>
+                        <Box h="40px" alignItems='center' display='flex' flexDirection='row'>
+                            <Text>No</Text>
+                            <Switch marginLeft={5} marginRight={5} size='lg' onChange={e => { setForm({ ...form, haveCar: e.target.checked }) }} />
+                            <Text>Yes</Text>
+                        </Box>
+                        <FormHelperText>Select if you have a car.</FormHelperText>
+                    </FormControl>
+                </SimpleGrid>
+                <Button
+                    onClick={handleSubmit}
+                    background="purple.700" color="white" size='lg' w="full" marginTop={10}>
+                    Submit form
+                </Button>
+            </div>
+
+        </>
     )
 }
--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,38 +1,43 @@
-import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
+import { SimpleGrid, Card, CardBody, Divider, FormControl, FormLabel, Heading, Image, Input, Stack, Text } from '@chakra-ui/react'
+import { User } from '../../App'
 
-export default function UserCard() {
+export default function UserCard({ user }: { user: User }) {
+    const { name } = user
     return (
         <Card maxW='sm'>
             <CardBody>
                 <Image
-                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                    alt='Green double couch with wooden legs'
+                    src='https://tjcc.com.br/wp-content/uploads/2021/11/shutterstock_1841020897-680x380.jpg'
+                    alt='User Photo'
                     borderRadius='lg'
                 />
                 <Stack mt='6' spacing='3'>
-                    <Heading size='md'>Living room Sofa</Heading>
+                    <Heading size='md'>{name}</Heading>
                     <Text>
-                        This sofa is perfect for modern tropical spaces, baroque inspired
-                        spaces, earthy toned spaces and for people who love a chic design with a
-                        sprinkle of vintage design.
-                    </Text>
-                    <Text color='blue.600' fontSize='2xl'>
-                        $450
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </Text>
                 </Stack>
+                <Divider />
+                <SimpleGrid columns={[1, 2]} spacing='40px' marginTop={5} marginBottom={5}>
+                    <FormControl>
+                        <FormLabel>Email address</FormLabel>
+                        <Input value={user["email"]} type='email' />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Phone</FormLabel>
+                        <Input value={user["phone"]} type='tel' />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Document</FormLabel>
+                        <Input value={user["document"]} type='text' />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Birthdate</FormLabel>
+                        <Input value={user["birthdate"]} type='date' />
+                    </FormControl>
+                </SimpleGrid>
             </CardBody>
             <Divider />
-            <CardFooter>
-                <ButtonGroup spacing='2'>
-                    <Button variant='solid' colorScheme='blue'>
-                        Buy now
-                    </Button>
-                    <Button variant='ghost' colorScheme='blue'>
-                        Add to cart
-                    </Button>
-                </ButtonGroup>
-            </CardFooter>
         </Card>
     )
 }

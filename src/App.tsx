5a3379f4153694ab59b0836264cd5b3c7964115e
Lucas Xavier <lucassxxavier@gmail.com>
2023-06-01 22:08:38 -0300

    feat: finishing form styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,35 @@ import React from 'react';
 import Form from './componets/Form';
 import './app.css';
 
+export type User = {
+  name: string;
+  email: string;
+  phone: string;
+  document: string;
+  haveCar: boolean;
+  birthdate: string;
+  gender: "F"|"M"
+}
+
+const formDefaultValue: User = {
+  name: '',
+  email: '',
+  phone: '',
+  document: '',
+  haveCar: false,
+  birthdate: '',
+  gender: "F",
+
+}
+
 function App() {
+  const [form, setForm] = React.useState<User>(formDefaultValue)
   return (
     <div className='container'>
-      <Form/>
+      <Form
+        form={form}
+        setForm={setForm}
+      />
     </div>
   );
 }
--- src/componets/Form/index.tsx
+++ src/componets/Form/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import './form.css'
-import { FormControl, Grid, FormLabel, Input, FormHelperText, Heading } from '@chakra-ui/react'
+import { FormControl, Grid, FormLabel, Input, FormHelperText, Switch,Heading, Button } from '@chakra-ui/react'
+import { User } from '../../App';
 
-export default function Form() {
+interface IFormProps {
+    form: User;
+    setForm: React.Dispatch<React.SetStateAction<User>>
+}
+
+export default function Form({ form, setForm }: IFormProps) {
+
     return (
         <div className='form-container'>
             <Heading as='h3' size='lg' marginBottom={10}>
-               Using Cypress
+                Using Cypress
             </Heading>
             <Grid templateColumns='repeat(2, 1fr)' gap={6}>
                 <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <FormLabel>Name</FormLabel>
+                    <Input type='text' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
@@ -20,26 +27,30 @@ export default function Form() {
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <FormLabel>Phone</FormLabel>
+                    <Input type='tel' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <FormLabel>Document</FormLabel>
+                    <Input type='text' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <FormLabel>Birthdate</FormLabel>
+                    <Input type='date' />
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
                 <FormControl>
-                    <FormLabel>Email address</FormLabel>
-                    <Input type='email' />
+                    <FormLabel>Birthdate</FormLabel>
+                    <Switch size='lg' h="40px"/>
                     <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
+                
             </Grid>
+            <Button background="purple.700" color="white" size='lg' w="full" marginTop={10}>
+                Submit form
+            </Button>
         </div>
     )
 }
